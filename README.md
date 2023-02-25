d05d31ea4ea2c3de9b225b8aeb32746a6825f2b1
Leabs <stevenleabo@gmail.com>
2023-02-24 21:18:29 -0500

    Scrape copy from webpage using BS4

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ To run the apps, you need to have python installed on your system. You can downl
 - Open the terminal and navigate to the directory where the repository is located.
 - Change directories into the chosen app.
 - Run the app using the command `python3 main.py`
+- Some apps may require you to install additional python modules. Use the requirements.txt file to install the modules by running `pip3 install -r requirements.txt`
 
 ## Apps
 
 - **Retirement Calculator** - Calculates how much you should have at your selected age of retirement with compounding interest. This also lets you calculate NYSLRS Tier 6 retirement benefits because that's what my wife has, lol.
 - **Password Generator** - Generates a random password with letters, numbers, symbols, and punctuation.
+- **Scrape All Copy** - Scrapes all the text from a website and saves it to a text file. Has requirements.txt file for installing the required modules.
 
 ## Contributing
 
---
+++ scrape-all-copy/main.py
@@ -0,0 +1,40 @@
+# This app asks the user for a web address and returns all copy from the website into a text file. This includes all headings and paragraphs. Note that there is no error handling for invalid web addresses.
+
+# Import the requests module
+import requests
+# Import beautiful soup 4
+from bs4 import BeautifulSoup
+
+# Define the main function
+
+
+def main():
+    # Print a blank line
+    print()
+    # Ask the user for a web address
+    web_address = input(
+        str("Enter a web address (example: https://stevenleabo.com): "))
+    # Get the web address
+    r = requests.get(web_address)
+    # Parse the web address
+    soup = BeautifulSoup(r.text, "html.parser")
+    # Find all the headings
+    headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
+    # Find all the paragraphs
+    paragraphs = soup.find_all("p")
+    # Open a text file
+    with open("copy.txt", "w") as f:
+        # Write the headings to the text file
+        for heading in headings:
+            f.write(heading.text.strip() + " ")
+        # Write the paragraphs to the text file
+        for paragraph in paragraphs:
+            f.write(paragraph.text.strip() + " ")
+    # Print a blank line
+    print()
+    # Print a message
+    print("All copy from the website has been saved to copy.txt")
+
+
+# Call the main function
+main()
